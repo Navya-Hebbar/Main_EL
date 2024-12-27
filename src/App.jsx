8b65d0e90df1881a5dc2ad92a57abaf1f5a05a7d
Navya-Hebbar <navyahebbar032005@gmail.com>
2024-12-27 10:57:14 +0530

    adding files into repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './home';
 import About from './about';
@@ -8,7 +7,6 @@ const App = () => {
   return (
     <Router>
       <div>
-
         {/* Switch for routing */}
         <Routes>
           <Route path="/" element={<Home />} />
@@ -22,28 +20,3 @@ const App = () => {
 };
 
 export default App;
-=======
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './home';
-import About from './about';
-import Profile from './Profile';
-
-const App = () => {
-  return (
-    <Router>
-      <div>
-
-        {/* Switch for routing */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
-
-export default App;
->>>>>>> master
